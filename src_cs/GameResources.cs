acf89807b49fea1bf69c203c90002c464fe87725
fubukix <calebteng@hotmail.com>
2019-04-17 15:03:27 +0800

    Fixed some coding bugs

--- src_cs/GameResources.cs
+++ src_cs/GameResources.cs
@@ -8,7 +8,8 @@ namespace Battleship
         private static void LoadFonts()
         {
             NewFont("ArialLarge", "arial.ttf", 80);
-            NewFont("Gameplay", "Gameplay.ttf",18);
+            NewFont("Gameplay", "Gameplay.ttf",16);
+            NewFont("Courier", "cour.ttf", 18);
             NewFont("CourierSmall", "cour.ttf", 8);
             NewFont("Menu", "ffaccess.ttf", 8);
         }
